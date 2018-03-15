a56de1b3abce0343e24d438f9770d3fc66cdf391
AdrianaUrsachi <adriana_ursachi3@yahoo.com>
2018-03-15 13:35:22 +0200

    Add files via upload

---
+++ Proj/Map/Scripts/Map.js
@@ -0,0 +1,10 @@
+﻿
+var map = L.map('map').setView([53.00, 9.00], 4);
+mapLink =
+    '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+L.tileLayer(
+    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; ' + mapLink + ' Contributors',
+        maxZoom: 20,
+    }).addTo(map);
+
