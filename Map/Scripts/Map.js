eda5a85bb124ac12194c0ff1b51933d1fe13817d
GabrielS5 <gabriel_schitcu@yahoo.com>
2018-03-18 12:49:24 +0200

    Update Map.js

--- Map/Scripts/Map.js
+++ Map/Scripts/Map.js
@@ -1,10 +1,9 @@
-﻿
 var map = L.map('map').setView([53.00, 9.00], 4);
 mapLink =
-    '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+    '<a href="http://openstreetmap.org"></a>';
 L.tileLayer(
     'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; ' + mapLink + ' Contributors',
+        attribution:  mapLink ,
         maxZoom: 20,
     }).addTo(map);
 
