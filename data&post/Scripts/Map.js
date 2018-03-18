183b31a07de2b5a13888a1c3e4d8a7bb22080673
GabrielS5 <gabriel_schitcu@yahoo.com>
2018-03-18 18:29:21 +0200

    Update Map.js

--- data&post/Scripts/Map.js
+++ data&post/Scripts/Map.js
@@ -1,3 +1,7 @@
+
+var lati=document.getElementById("latitudine");
+var long=document.getElementById("longitudine");
+
 var map = L.map('map').setView([53.00, 9.00], 4);
 mapLink =
     '<a href="http://openstreetmap.org"></a>';
@@ -6,4 +10,14 @@ L.tileLayer(
         attribution:  mapLink ,
         maxZoom: 20,
     }).addTo(map);
+	
+	
+	function addMarker(){
+	var marker = L.marker([parseFloat(lati.value), parseFloat(long.value)]).addTo(map);
+	}
+	function updateMap(){
+	var lat=parseFloat(lati.value)||0;
+	var lon=parseFloat(long.value)||0;
+	map.panTo(new L.LatLng(lat, lon));
+}
 
